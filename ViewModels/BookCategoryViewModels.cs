9c851dfe60b625ac651a83a8078748a55f15547f
minhquoc1423@gmail.com <minhquoc1423@gmail.com>
2022-11-29 19:34:40 +0700

    Move BookCategoryViewModels.cs to ViewModels folder

--- ViewModels/BookCategoryViewModels.cs
+++ ViewModels/BookCategoryViewModels.cs
@@ -1,10 +1,10 @@
 using Microsoft.AspNetCore.Mvc.Rendering;
 
-namespace FPTBook.Models;
+namespace FPTBook.ViewModels;
 
 public class BookCategoryViewModels
 {
-    public List<Book> Books { get; set; }
+    public List<Models.Book> Books { get; set; }
     public SelectList? Categories { get; set; }
     public string? BookCategory { get; set; }
     public string? SearchString { get; set; }
