4f39c581df0fa8e77975272634338a135219b526
MinhTri24 <tripmgcd201632@fpt.edu.vn>
2022-12-06 08:56:40 +0700

    update category request model

--- Data/Migrations/20221206015526_UpdateCategoryRequestModel.Designer.cs
+++ Data/Migrations/20221206015526_UpdateCategoryRequestModel.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using FPTBook.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FPTBook.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221206015526_UpdateCategoryRequestModel")]
+    partial class UpdateCategoryRequestModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.10");
--- Models/CategoryRequest.cs
+++ Models/CategoryRequest.cs
@@ -7,11 +7,8 @@ namespace FPTBook.Models;
 public class CategoryRequest
 {
     public int Id { get; set; }
-    [DisplayName("User Id:")]
-    [ForeignKey("ApplicationUser")]
-    public string UserId { get; set; }
-    public ApplicationUser ApplicationUser { get; set; }
-    public string Content { get; set; }
+    [DisplayName("Category")]
+    public string Name { get; set; }
     public bool Is_Approved { get; set; }
     public DateTime CreatedAt { get; set; }
 
