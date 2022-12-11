5a550f00e5886c9a483adf960a01b19af7df5100
MinhTri24 <tripmgcd201632@fpt.edu.vn>
2022-12-11 23:28:56 +0700

    Add UI

--- ViewModels/UsersDetail.cs
+++ ViewModels/UsersDetail.cs
@@ -1,9 +1,11 @@
-﻿using FPTBook.Models;
+﻿using System.Collections;
+using FPTBook.Models;
 
 namespace FPTBook.ViewModels;
 
 public class UsersDetail
 {
-    public List<ApplicationUser> Users { get; set; }
+    public IEnumerable<ApplicationUser> Users { get; set; }
     public string SearchString { get; set; }
+
 }   
